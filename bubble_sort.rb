fa8caf92b2900018f84c138d7e3322f1f9f11509
joshuaivie <joshua@lifeonline.com.ng>
2021-04-08 11:33:26 +0100

    Add #bubble_sort_by

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -12,3 +12,18 @@ def bubble_sort(arr)
   bubble_sort(arr) if swapped
   arr
 end
+
+# Bubble Sort using Yield and Blocks ================
+def bubble_sort_by(arr)
+  range = arr.length - 1
+  swapped = false
+
+  (0...range).each do |i|
+    if yield(arr[i], arr[i + 1]) >= 1
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
+      swapped = true
+    end
+  end
+  bubble_sort_by(arr) { |a, b| a.length - b.length } if swapped
+  arr
+end
